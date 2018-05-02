f6918a629689cc502db87c144e2896883c032ba8
holenet <holenet6147@naver.com>
2018-05-02 15:46:02 +0900

    create models in ttrs app (#2)
    created and registered models in ttrs app;
      Student
      Course
      Lecture
      Evaluation
      TimeTable
      TimeSlot
      Classroom
      College
      Department
      Major

--- backend/ttrs/urls.py
+++ backend/ttrs/urls.py
@@ -4,4 +4,23 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test),
+
+    # TODO: implements views for these urls
+    #     path('students/'),
+    #     path('students/signin/'),
+    #     path('students/signup/'),
+    #     path('students/validate/'),
+    #     path('students/withdraw/'),
+    #     path('students/not-recommend/'),
+    #
+    #     path('timetables/<str:type>/'),
+    #     path('timetables/recommend/'),
+    #     path('timetables/send/<str:student>'),
+    #
+    #     path('lectures'),
+    #
+    #     path('majors'),
+    #
+    #     path('evaluations/lecture/<int:lecture_id>'),
+    #     path('evaluations/<int:evaluations_id>'),
 ]
