cf50b34625f6a7631e81c02348d08bfbf47aa023
holenet <holenet6147@naver.com>
2018-05-03 21:41:59 +0900

    add year, semester field to Crawler model   be able to run a crawler with year, semester arguments

--- backend/crawler/models.py
+++ backend/crawler/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Crawler(models.Model):
     started = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=100)
+    year = models.CharField(max_length=10)
+    semester = models.CharField(max_length=10)
+    status = models.TextField()
     cancel_flag = models.BooleanField(default=False)
 
     def __str__(self):
-        return 'Crawler {} - {}'.format(self.id, self.status)
+        return '{}-{} {}'.format(self.year, self.semester, self.status)
