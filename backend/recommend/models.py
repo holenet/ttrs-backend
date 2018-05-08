64fd3e69641570476d4cc2a22d048a4e2b49f411
lameinthebox <lameinthebox@snu.ac.kr>
2018-05-08 16:29:01 +0900

    app recommend; initial commit

---
+++ backend/recommend/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+class Recommender(models.model):
---
+++ backend/recommend/views.py
@@ -0,0 +1,29 @@
+import threading
+
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+
+from .models import Crawler
+from .serializers import CrawlerSerializer, CrawlerDetailSerializer
+
+from .crawler import run
+
+
+class CrawlerList(generics.ListCreateAPIView):
+    queryset = Crawler.objects.all()
+    serializer_class = CrawlerSerializer
+    permission_classes = (IsAdminUser, )
+
+    def perform_create(self, serializer):
+        crawler = serializer.save(status='creating')
+        thread = threading.Thread(target=run, args=(crawler,))
+        thread.start()
+
+
+class CrawlerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Crawler.objects.all()
+    serializer_class = CrawlerDetailSerializer
+    permission_classes = (IsAdminUser, )
+
+    def perform_update(self, serializer):
+        serializer.save(cancel_flag=True)
