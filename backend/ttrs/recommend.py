3632cedcc100af497615090568ec5798622a9fc0
lameinthebox <lameinthebox@snu.ac.kr>
2018-05-11 00:50:13 +0900

    minor fix

--- backend/ttrs/recommend.py
+++ backend/ttrs/recommend.py
@@ -1,38 +1,41 @@
 import json
 import random
 
+
 def recommend(_options):
     try:
         options = json.loads(_options)
-        print(options)
-
-        student_id = int(options.get('student_id'))
-        expected_credit = int(options.get('expected_credit'))
 
         recommends = {}
         num_recommends = 3
         for i in range(num_recommends):
-            lectures = build_timetable(student_id, expected_credit)
+            lectures = build_timetable(options)
             score = get_score(lectures)
-
-            print('tt {}:'.format(i), lectures, score)
 
             recommends['tt {}'.format(i)] = (lectures, score)
 
-        print(json.dumps(recommends))
         return json.dumps(recommends)
 
     except Exception as e:
         print(e)
 
 
-def build_timetable(student_id, expected_credit):
+def build_timetable(options):
     lectures = []
+
+    if 'student_id' in options:
+        student_id = int(options.get('student_id'))
+        # grab some info about student from db
+
+    expected_credit = int(options.get('expected_credit')) if 'expected_credit' in options else 18
+        
+    # some recursive method to construct tt?
     for j in range(expected_credit):
         lecture_id = random.randrange(1,100)
         lectures.append(lecture_id)
 
     return lectures
+
 
 def get_score(lectures):
     return random.randrange(1,100)
