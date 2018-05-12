bfa54e5fc4cf59c26b82d2d3761f159d64c7e535
lameinthebox <lameinthebox@snu.ac.kr>
2018-05-12 20:24:57 +0900

    minor fix based on code review

--- test/clean.py
+++ test/clean.py
@@ -3,64 +3,38 @@ def clean_crawlers():
     from manager.models import Crawler
 
     crawlers = Crawler.objects.all()
-    print('Crawler:', crawlers)
-
-    for crawler in crawlers:
-        try:
-            print('Deleting crawler {}...'.format(crawler.id))
-            crawler.delete()
-
-        except Exception as e:
-            print(e)
+    print('Deleting Crawler instances:', crawlers)
+    crawlers.delete()
 
 
 def clean_students():
     from ttrs.models import Student
     
     students = Student.objects.all()
-    print('Student:', students)
-
-    for student in students:
-        try:
-            print('Deleting student {}...'.format(student.username))
-            student.delete()
-
-        except Exception as e:
-            print(e)
+    print('Deleting Student instances:', students)
+    students.delete()
 
 
 def clean_colleges():
     from ttrs.models import College
 
     colleges = College.objects.all()
-    print('College:', colleges)
-
-    for college in colleges:
-        try:
-            print('Deleting college {}...'.format(college.id))
-            college.delete()
-
-        except Exception as e:
-            print(e)
+    print('Deleting College instances:', colleges)
+    colleges.delete()
 
 
 def clean_classrooms():
     from ttrs.models import Classroom
 
     classrooms = Classroom.objects.all()
-    print('Classroom:', classrooms)
-
-    for classroom in classrooms:
-        try:
-            print('Deleting classroom {}...'.format(classroom.id))
-            classroom.delete()
-
-        except Exception as e:
-            print(e)
+    print('Deleting Classroom instances:', classrooms)
+    classrooms.delete()
 
 
-print('Cleaning models...')
+print('\033[1m'+'='*15+' Initialize '+'='*15+'\033[0m')
 clean_crawlers()
 clean_students()
 clean_colleges()
 clean_classrooms()
+print()
+print('Successfully cleaned model instances.')
