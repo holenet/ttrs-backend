242656d5ac384a164994a7e80f586d4facd1d47a
holenet <holenet6147@naver.com>
2018-05-12 22:43:10 +0900

    settings for django-cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
     'manager.apps.ManagerConfig',
     'ttrs.apps.TtrsConfig',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
