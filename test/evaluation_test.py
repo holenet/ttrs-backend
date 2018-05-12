2f93b6991d8d9989e9182c8b7f935ca27253dc7c
lameinthebox <lameinthebox@snu.ac.kr>
2018-05-12 23:16:11 +0900

    improvement on test code; evaluation_test tests for detail

--- test/evaluation_test.py
+++ test/evaluation_test.py
@@ -4,7 +4,7 @@ def evaluation_test(uid, upwd):
     base = 'http://localhost:8000/ttrs/evaluations/'
 
     import json, requests
-    from ttrs.models import Lecture
+    from ttrs.models import Lecture, Evaluation
 
     print('\ntesting '+'\033[1m'+base+'\033[0m'+'...')
     for i in range(1,4):
@@ -34,3 +34,38 @@ def evaluation_test(uid, upwd):
     errors = json.loads(res.text)
     for error in errors:
         print(error, errors.get(error))
+
+    print('\ntesting '+'\033[1m'+base+'\033[0m')
+    res = requests.get(base, auth=auth)
+    if res.status_code == 200:
+        print('Successfully accessed Evaluation list.')
+    else:
+        print('Error accessing Evaluation list.')
+        print(res.content)
+
+    filter = base+'?id='+str(Lecture.objects.all()[0].id)
+    print('\ntesting '+'\033[1m'+filter+'\033[0m')
+    res = requests.get(filter, auth=auth)
+    if res.status_code == 200:
+        print('Successfully accessed Evaluation list with payload.')
+    else:
+        print('Error accessing Evaluation list with payload.')
+        print(res.content)
+
+    filter = base+'?lecture__course__name__contains=글'
+    print('\ntesting '+'\033[1m'+filter+'\033[0m')
+    res = requests.get(filter, auth=auth)
+    if res.status_code == 200:
+        print('Successfully accessed Evaluation list with payload.')
+    else:
+        print('Error accessing Evaluation list with payload.')
+        print(res.content)
+    
+    detail = base+str(Evaluation.objects.all()[0].id)+'/'
+    print('\ntesting '+'\033[1m'+detail+'\033[0m')
+    res = requests.get(detail, auth=auth)
+    if res.status_code == 200:
+        print('Successfully accessed Evaluation detail.')
+    else:
+        print('Error accessing Evaluation detail.')
+        print(res.content)
